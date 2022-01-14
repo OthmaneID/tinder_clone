27613018c41a0614aefe34fad746d2465b4d4b9a
OthmaneID <othmanid5@users.noreply.github.com>
2022-01-14 19:52:34 +0100

    frontend Done Starting BackEnd  commit

---
+++ tinder-backEnd/server.js
@@ -0,0 +1,24 @@
+import express from "express"
+import mongoose from "mongoose";
+
+
+// App config
+const app = express()
+const port = process.env.PORT || 8001
+
+// Middlewares
+
+// DB config
+
+// API Endpoints
+app.get('/', (req, res) => {
+    // Means Okay
+    res.status(200).send("Hello World !!!");
+    // console.log("Hello World");
+})
+
+// Listener
+
+app.listen(port, () => {
+    console.log(`listening on localhost : ${port}`);
+})
