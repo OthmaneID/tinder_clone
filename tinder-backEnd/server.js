826eedb64859a6ab42e107b439809d97a21a3cf6
OthmaneID <othmanid5@users.noreply.github.com>
2022-01-14 20:11:07 +0100

    db configuration

--- tinder-backEnd/server.js
+++ tinder-backEnd/server.js
@@ -5,10 +5,15 @@ import mongoose from "mongoose";
 // App config
 const app = express()
 const port = process.env.PORT || 8001
-
+const connectionURL ="mongodb+srv://<addmin>:idboufker123Tamanar@cluster0.3kiib.mongodb.net/tinder-db?retryWrites=true&w=majority"
 // Middlewares
 
 // DB config
+mongoose.connect(connectionURL,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+});
 
 // API Endpoints
 app.get('/', (req, res) => {
